502fb22ab3e53d80b3ec0af03ae9669dccb806f9
ziwenhahaha <44536843+ziwenhahaha@users.noreply.github.com>
2025-07-19 18:18:33 +0800

    Update setup_ollama.sh

--- setup_ollama.sh
+++ setup_ollama.sh
@@ -28,7 +28,7 @@ OLLAMA_INSTALL_DIR="/data1/$USERNAME/ollama"
 if [ ! -f "$OLLAMA_TGZ" ]; then
     print_progress "开始下载 Ollama"
     mkdir -p /data1/$USERNAME/tmp
-    wget -c --no-check-certificate -O "$OLLAMA_TGZ" "https://github.com/ollama/ollama/releases/download/v0.9.6/ollama-linux-amd64.tgz"
+    wget -c --no-check-certificate -O "$OLLAMA_TGZ" "https://github.com/ollama/ollama/releases/download/v0.9.0/ollama-linux-amd64.tgz"
     print_progress "Ollama 下载完成"
 else
     print_progress "Ollama 已存在，跳过下载"
